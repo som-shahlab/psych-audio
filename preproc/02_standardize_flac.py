d575b5f7441b851f83d7e4d6a2709693c21d4937
Albert Haque <ahaque@cs.stanford.edu>
2019-03-04 02:21:32 -0800

    Initial version of flac standardization and joining.

---
+++ preproc/02_standardize_flac.py
@@ -0,0 +1,47 @@
+"""
+This script takes existing flac files and standardizes them according
+to our metadata tsv file. This includes joining multiple flac files into
+a single audio file (e.g., a single therapy session was split into two audio files).
+
+Make sure to have finsihed running `preproc/01_generate_flac.py` before running this script.
+"""
+import os
+import shutil
+import argparse
+import pandas as pd
+import util
+import config
+
+
+def main(args):
+    meta = pd.read_csv(config.meta_fqn, sep='\t')
+
+    # For each metadata row, copy and rename the audio file.
+    for i, row in meta.iterrows():
+        hash = row['hash']
+        path = str(row['audio_path'])
+
+        if ' ' in path:
+            print('Found space in file')
+
+        if path == 'nan':
+            continue
+        elif ';' in path:
+            # We have 2 paths. Need to concat.
+            pass
+        else:
+            filename = util.remove_extension(os.path.basename(path))
+            source_fqn = os.path.join(args.input_dir, f'{filename}.flac')
+            dest_fqn = os.path.join(args.output_dir, f'{hash}.flac')
+            shutil.copy(source_fqn, dest_fqn)
+            print(source_fqn, dest_fqn)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', type=str,
+                        help='Directory which contains all flac files.')
+    parser.add_argument('output_dir', type=str,
+                        help='Location to place the new flac files.')
+    args = parser.parse_args()
+    main(args)
---
+++ preproc/util.py
@@ -0,0 +1,59 @@
+"""
+This file contains helper functions for the other pre-processing files.
+"""
+import os
+
+
+def metadata_file_is_clean(fqn: str) -> bool:
+    """
+    Checks whether teh metadata file is valid.
+    - All rows are tab-delimited.
+    - All rows contain valid audio files. Note that some rows have two associated files.
+    - All rows contain a valid SHA256 hash.
+    - All rows have 36 columns.
+
+    :param fqn: Full path to the metadata file.
+    :return: True if metadata file is correct. False otherwise.
+    """
+    # Check if it exists.
+    if not os.path.exists(fqn):
+        print(f'File does not exist: {fqn}')
+        return False
+
+    # Open and check each line.
+    with open(fqn, 'r') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            # Skip the first line since it contains headers.
+            if i == 0:
+                continue
+
+            # Check if line is valid.
+            tokens = line.strip().split('\t')
+            if len(tokens) != 36:
+                print(f'Line {i+1} does not have 36 columns.')
+                return False
+
+            fqns = tokens[32].split(';')
+            # It is possible for this row to have zero filenames.
+            if len(fqns) == 1 and fqns[0] == '':
+                continue
+            # If this row has non-empty audio filenames.
+            for fqn in fqns:
+                if not os.path.exists(fqn):
+                    print(f'Line {i+1} audio does not exist: {fqn}')
+                    return False
+
+    return True
+
+
+def remove_extension(filename: str) -> str:
+    """
+    Removes the mp3 or wma extension from a string.
+
+    :param filename: Filename to parse.
+    :return: Filename but without the extension.
+    """
+    for ext in ['.wma', '.WMA', '.mp3', '.MP3']:
+        filename = filename.replace(ext, '')
+    return filename
