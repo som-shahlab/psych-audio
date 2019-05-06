9b4aaad8dc5c75d505ba57614c54230257a0fcca
Albert Haque <albert.haque@gmail.com>
2019-05-06 12:56:34 +0100

    Random BERT embeddings.

--- evaluation/embeddings/05_random_embeddings.py
+++ evaluation/embeddings/05_random_embeddings.py
@@ -12,6 +12,7 @@ from typing import *
 import scipy.spatial.distance
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+from bert_serving.client import BertClient
 
 import evaluation.util
 from evaluation.embeddings import config
@@ -34,25 +35,26 @@ def main(args):
 	ls = os.listdir(embedding_dir)
 	fqn = ''
 	has_file = False
-	if len(ls) == 0:
-		generate_embeddings(args)
-	else:
+	if len(ls) > 0:
 		# Make sure we have a npy for this embedding.
 		for filename in ls:
 			if args.embedding_name == filename[:-4]:
 				fqn = os.path.join(embedding_dir, filename)
 				has_file = True
+
+	if not has_file:
+		generate_embeddings(args)
 	
 	# Compute distances.
 	if has_file:
 		print(f'Loading: {fqn}')
-		A = np.load(fqn)[:200]
+		A = np.load(fqn)
 		cosine = flat_pairwise(A, 'cosine')
 		euclidean = flat_pairwise(A, 'euclidean')
 		wasserstein = flat_wasserstein(A)
 
 		# Create the subpanels.
-		fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+		fig, axes = plt.subplots(1, 2, figsize=(20, 9))
 		axes[0].hist(cosine, 100, density=True, facecolor='g', alpha=0.75, label='cosine')
 		axes[0].hist(euclidean, 100, density=True, facecolor='b', alpha=0.75, label='euclidean')
 		axes[0].hist(wasserstein, 100, density=True, facecolor='r', alpha=0.75, label='wasserstein')
@@ -89,11 +91,7 @@ def flat_wasserstein(A: np.ndarray):
 		for j in range(i + 1, len(A)):
 			flat[idx] = scipy.stats.wasserstein_distance(A[i], A[j])
 			idx += 1
-
-	print(f'------ wasserstein ------')
-	print(f'Mean: {flat.mean():.4f}')
-	print(f'Std: {flat.std():.4f}')
-	print(f'n: {len(flat)}')
+	print_metrics(flat, 'wasserstein')
 	return flat
 
 
@@ -102,23 +100,34 @@ def flat_pairwise(A: np.ndarray, metric: str):
 	dists = scipy.spatial.distance.cdist(A, A, metric=metric)
 	idx1, idx2 = np.nonzero(np.tril(dists))  # Only time dist=0 is if exact same sentence, which we want to discard.
 	flat = dists[idx1, idx2]
-	print(f'------ {metric} ------')
-	print(f'Mean: {flat.mean():.4f}')
-	print(f'Std: {flat.std():.4f}')
-	print(f'n: {len(flat)}')
+	print_metrics(flat, metric)
 	return flat
 
 
-def generate_embeddings(args):
+def print_metrics(arr: np.ndarray, text: str):
+	"""Prints various metrics for an array."""
+	print(f'------ {text} ------')
+	print(f'Mean: {arr.mean():.4f}')
+	print(f'Std: {arr.std():.4f}')
+	print(f'n: {len(arr)}')
+
+
+def generate_embeddings(args, bc=None):
 	"""
 	Generates random sentences and embeddings and stores them in `embedding_dir`.
 	
 	Args:
 		args: Argparse namespace.
+		bc: Bert Client.
 	"""
 	# Load the embedding model.
-	print(f'Loading the {args.embedding_name} model...')
-	model, keys = evaluation.embeddings.util.load_embedding_model(args.embedding_name)
+	if args.embedding_name == 'bert':
+		print('Creating BERT client...')
+		bc = BertClient(check_length=False)
+		print('Done.')
+	else:
+		print(f'Loading the {args.embedding_name} model...')
+		model, keys = evaluation.embeddings.util.load_embedding_model(args.embedding_name)
 
 	# Load the vocabulary.
 	with open(word_file, 'r') as f:
@@ -130,13 +139,17 @@ def generate_embeddings(args):
 		s = generate_sentence(vocab)
 		sentences.append(s)
 
-	embeddings = np.zeros((args.n, config.F[args.embedding_name]), np.float32)
-	for i, s in enumerate(sentences):
-		embed = evaluation.embeddings.util.encode_from_dict(args.embedding_name, model, keys, s)
-		if embed is None:
-			print(f'No embedding: {s}')
-		else:
-			embeddings[i] = embed
+	if args.embedding_name == 'bert':
+		embeddings = bc.encode(sentences)
+
+	if args.embedding_name in ['word2vec', 'glove']:
+		embeddings = np.zeros((args.n, config.F[args.embedding_name]), np.float32)
+		for i, s in enumerate(sentences):
+			embed = evaluation.embeddings.util.encode_from_dict(args.embedding_name, model, keys, s)
+			if embed is None:
+				print(f'No embedding: {s}')
+			else:
+				embeddings[i] = embed
 
 	fqn = os.path.join(embedding_dir, f'{args.embedding_name}.npy')
 	np.save(fqn, embeddings)
@@ -166,7 +179,9 @@ def generate_sentence(vocab: List[str]) -> str:
 
 
 if __name__ == '__main__':
+	plt.style.use('ggplot')
+	plt.rcParams.update({'font.size': 20})
 	parser = argparse.ArgumentParser()
 	parser.add_argument('n', type=int, default=100, help='Number of sentences to generate.')
-	parser.add_argument('embedding_name', type=str, choices=['word2vec', 'glove'])
+	parser.add_argument('embedding_name', type=str, choices=['word2vec', 'glove', 'bert'])
 	main(parser.parse_args())
