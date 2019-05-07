e3b335667ff7f1f6fdbdbeae52565383857da8ed
Albert Haque <albert.haque@gmail.com>
2019-05-07 16:36:40 +0100

    Upload with exclusion rule.

--- gcloud/01_upload.py
+++ gcloud/01_upload.py
@@ -7,27 +7,42 @@ Prerequisites:
 """
 import os
 import sys
-import config
+import gcloud.config
 import argparse
 from tqdm import tqdm
 from google.cloud import storage
+from typing import List
 
 
 def main(args):
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(config.BUCKET_NAME)
+	storage_client = storage.Client()
+	bucket = storage_client.get_bucket(gcloud.config.BUCKET_NAME)
 
-    print(f'Uploading files from: {args.data_dir}')
-    for filename in tqdm(os.listdir(args.data_dir)):
-        fqn = os.path.join(args.data_dir, filename)
-        blob = bucket.blob(filename)  # Create the target blob/file on the bucket.
-        blob.upload_from_filename(fqn)  # Upload the local file into the target blob/file.
+	# Determine files to upload.
+	ls: List[str] = os.listdir(args.data_dir)
+	print(f'Data Dir: {args.data_dir}')
+
+	if args.exclude_dir != '':
+		before = len(ls)
+		print(f'Exclusion Dir: {args.exclude_dir}')
+		exclude_ls = os.listdir(args.exclude_dir)
+		ls = list(set(ls) - set(exclude_ls))
+		print(f'Applied Exclusion Rule: {before} -> {len(ls)}')
+
+
+	print(f'Uploading files from: {args.data_dir}')
+	for i in tqdm(range(len(ls))):
+		filename = ls[i]
+		fqn = os.path.join(args.data_dir, filename)
+		blob = bucket.blob(filename)  # Create the target blob/file on the bucket.
+		blob.upload_from_filename(fqn)  # Upload the local file into the target blob/file.
 
 
 if __name__ == '__main__':
-    # Google python API requires we set the OS environment variable.
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.KEY
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_dir', type=str, help='Location of files to upload.')
-    args = parser.parse_args()
-    main(args)
+	# Google python API requires we set the OS environment variable.
+	os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = gcloud.config.KEY
+	parser = argparse.ArgumentParser()
+	parser.add_argument('data_dir', type=str, help='Location of files to upload.')
+	parser.add_argument('exclude_dir', type=str, default='', help='Location of folder containing exclude files.')
+	args = parser.parse_args()
+	main(args)
