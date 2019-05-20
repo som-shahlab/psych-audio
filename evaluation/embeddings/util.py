efb40f398bf0600193180f2534aeba972df856a9
Albert Haque <albert.haque@gmail.com>
2019-05-20 11:12:07 -0700

    Moved shared dist functions to util.py.

--- evaluation/embeddings/util.py
+++ evaluation/embeddings/util.py
@@ -4,6 +4,7 @@ Contains various util functions for loading and computing embeddings.
 import gensim
 import numpy as np
 from typing import *
+import matplotlib.pyplot as plt
 import evaluation.util
 import evaluation.embeddings.util
 from evaluation.embeddings import config
@@ -133,7 +134,7 @@ def encode_from_dict(embedding_name: str, model: Dict[str, np.ndarray], keys: Se
 	words = sentence.split(' ')
 	# Count the number of words for which we have an embedding.
 	count = 0
-	for i, word in enumerate(words):
+	for word in words:
 		if word in keys:
 			count += 1
 	if count == 0:
@@ -170,3 +171,41 @@ def load_glove(path: str) -> Dict[str, np.ndarray]:
 			vec = np.array([float(x) for x in tokens[1:]])
 			model[word] = vec
 	return model
+
+
+def print_metrics(arr: np.ndarray, text: str):
+	"""Prints various metrics for an array."""
+	print(f'------ {text} ------')
+	print(f'Mean: {arr.mean():.4f}')
+	print(f'Std: {arr.std():.4f}')
+	print(f'n: {len(arr)}')
+
+
+def plot_histogram(fqn: str, random_dists: np.ndarray, corpus_dists: np.ndarray):
+	"""
+	Plots a histogram of the random and corpus distances.
+	
+	Args:
+		fqn (str): Where to save the output figure.
+		random_dists (np.ndarray): (N,) vector of distances between random sentences.
+		corpus_dists (np.ndarray): (N,) vector of distances between corpus sentences.
+	"""
+	# For histogram to sum to 1, histogram requires integer=1 width bins. Our distances
+	# are often very small. Therefore, we need to scale the distances (temporarily) to compute hist.
+	n_bins = 30
+	random_dists = (random_dists * n_bins).astype(np.int64)
+	corpus_dists = (corpus_dists * n_bins).astype(np.int64)
+
+	# Need to manually specify bins otherwise the histogram will not sum to 1.
+	min_val = min(random_dists.min(), corpus_dists.min())
+	max_val = max(random_dists.max(), corpus_dists.max())
+	bins = np.arange(min_val, max_val)
+
+	# Create the histogram.
+	_, axes = plt.subplots(1, 1, figsize=(16, 10))
+	axes.hist(random_dists, bins=bins, density=True, facecolor='g', alpha=0.6, label='Random')
+	axes.hist(corpus_dists, bins=bins, density=True, facecolor='b', alpha=0.6, label='Corpus')
+	axes.set_xlabel('Distance')
+	axes.set_ylabel('Probability')
+	axes.legend()
+	plt.savefig(fqn, dpi=300)
