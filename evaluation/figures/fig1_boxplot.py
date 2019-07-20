fac756b7da909bc7487d94ae1bcf5a48b41f7e65
Albert Haque <albert@alberthaque.com>
2019-07-20 14:04:40 -0700

    Boxplot complete.

--- evaluation/figures/fig1_boxplot.py
+++ evaluation/figures/fig1_boxplot.py
@@ -36,10 +36,10 @@ def main():
         male = table2[table2["gender"] == "Male"][metric].values
         female = table2[table2["gender"] == "Female"][metric].values
         data[metric]["key"] += ["male"] * len(male)
-        data[metric]["lr"] += ["2"] * len(male)
+        data[metric]["lr"] += ["1"] * len(male)
         data[metric]["group"] += ["group1"] * len(male)
         data[metric]["values"] += list(male)
-        data[metric]["lr"] += ["1"] * len(female)
+        data[metric]["lr"] += ["2"] * len(female)
         data[metric]["group"] += ["group1"] * len(female)
         data[metric]["key"] += ["female"] * len(female)
         data[metric]["values"] += list(female)
@@ -47,36 +47,58 @@ def main():
         # Load the speaker information.
         therapist = table2[table2["speaker"] == "T"][metric].values
         patient = table2[table2["speaker"] == "P"][metric].values
-        data[metric]["key"] += ["therapist"] * len(therapist)
-        data[metric]["group"] += ["group2"] * len(therapist)
-        data[metric]["lr"] += ["2"] * len(therapist)
-        data[metric]["values"] += list(therapist)
         data[metric]["group"] += ["group2"] * len(patient)
         data[metric]["key"] += ["patient"] * len(patient)
         data[metric]["lr"] += ["1"] * len(patient)
         data[metric]["values"] += list(patient)
+        data[metric]["key"] += ["therapist"] * len(therapist)
+        data[metric]["group"] += ["group2"] * len(therapist)
+        data[metric]["lr"] += ["2"] * len(therapist)
+        data[metric]["values"] += list(therapist)
 
+    # Convert WER from decimal to percentage.
+    data["WER"]["values"] = list(np.asarray(data["WER"]["values"]) * 100)
+
+    plot("WER", data)
+    plot("EMD", data)
+
+
+def plot(metric: str, data: pd.DataFrame):
+    """
+    Creates a single plot for a specific metric.
+    
+    Args:
+        metric (str): ''WER' or 'EMD'
+    """
     # Create the actual dataframe.
-    df = pd.DataFrame(data=data["WER"])
+    df = pd.DataFrame(data=data[metric])
 
     # Plot the histogram.
     sns.set(style="ticks", palette="pastel")
-    ax = sns.catplot(
-        data=df, x="lr", y="values", col="group", kind="box", aspect=0.4
+    fig, ax = plt.subplots(1, 1, figsize=(30, 30))
+    sns.catplot(
+        data=df,
+        x="lr",
+        y="values",
+        col="group",
+        kind="box",
+        aspect=0.4,
+        palette=sns.color_palette("Blues", n_colors=2),
     )
-    plt.savefig("boxplot.png")
-    ax.set(ylim=(0, 0.8))
+    # ax.set_xlabel("")
     sns.despine(offset=10, trim=True)
+    plt.savefig(f"boxplot_{metric}.png", dpi=300)
+    # ax.set(ylim=(0, 0.8))
 
     # Violin plot.
-    plt.clf()
-    sns.set(style="whitegrid")
-    ax = sns.violinplot(
-        x="group", y="values", hue="lr", data=df, palette="muted", split=True
-    )
-    ax.set(ylim=(0, 0.8))
-    sns.despine(offset=10, trim=True)
-    plt.savefig("violin.png")
+    # plt.clf()
+    # sns.set(style="whitegrid")
+    # ax = sns.violinplot(
+    #     x="group", y="values", hue="lr", data=df, palette="muted", split=True
+    # )
+    # ax.set(ylim=(0, 0.8))
+    # sns.despine(offset=10, trim=True)
+    # plt.savefig("violin.png")
 
 
 if __name__ == "__main__":
