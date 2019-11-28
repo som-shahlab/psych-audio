5db3fe22750b07e8fc388f918859bbb8a8289f69
Albert Haque <albert@alberthaque.com>
2019-11-27 23:04:06 -0500

    Removed unnecessary print statements.

--- evaluation/stats.py
+++ evaluation/stats.py
@@ -21,12 +21,10 @@ def difference_test(labels: List, arr0: np.ndarray, arr1: np.ndarray) -> Dict:
     statistic0, pvalue0 = scipy.stats.shapiro(arr0)
     statistic1, pvalue1 = scipy.stats.shapiro(arr1)
 
-    print("------------ NORMALITY TESTS ------------")
     print("Shapiro-Wilk")
     print(f"\t{labels[0]} w: {statistic0:.4f}\tP: {pvalue0:.3e}")
     print(f"\t{labels[1]} w: {statistic1:.4f}\tP: {pvalue1:.3e}")
 
-    print("------------ VARIANCE TESTS ------------")
     print("Bartlett (Normally Distributed)")
     stat, pval = scipy.stats.bartlett(arr0, arr1)
     print(f"\tT: {stat:.4f}\tP: {pval:.3e}")
@@ -35,7 +33,6 @@ def difference_test(labels: List, arr0: np.ndarray, arr1: np.ndarray) -> Dict:
     stat, pval = scipy.stats.levene(arr0, arr1)
     print(f"\tw: {stat:.4f}\tP: {pval:.3e}")
 
-    print("------------ DIFFERENCE TESTS ------------")
     print("Student t-test (Equal Var; Normally Distributed)")
     stat, pval = scipy.stats.ttest_ind(arr0, arr1, equal_var=True)
     print(f"\tt: {stat:.4f}\tP: {pval:.3e}")
