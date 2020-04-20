e71a1fdacc3bd0664a1a8c89c2667aa6c8810b61
Albert Haque <albert@alberthaque.com>
2020-04-19 21:03:00 -0700

    Removed folder paths.

--- evaluation/self_harm/config.py
+++ evaluation/self_harm/config.py
@@ -1,17 +1,17 @@
 # Location of the self-harm annotation file.
-LABEL_FILE = "/vol0/psych_audio/gold-transcripts/self-harm-annotations.txt"
+LABEL_FILE = "gold-transcripts/self-harm-annotations.txt"
 
 # Location of gold TXT with the hash filename.
-TXT_DIR = "/vol0/psych_audio/gold-transcripts/gold-final_2019-04-08_anno"
+TXT_DIR = "gold-transcripts/gold-final_2019-04-08_anno"
 
 # Location of the word-level GT.json and pred.json files.
-JASA_DIR = "/vol0/psych_audio/jasa_format/v7s"
+JASA_DIR = "jasa_format/v7s"
 
 # Output directory to store the TSV files.
-OUT_DIR = "/vol0/psych_audio/ahaque/self_harm/alignment"
+OUT_DIR = "self_harm/alignment"
 
 # Self harm event ID to speaker mapping.
-SPEAKER_FILE = "/vol0/psych_audio/ahaque/self_harm/speakers.txt"
+SPEAKER_FILE = "self_harm/speakers.txt"
 
 # Self harm event ID to speaker mapping.
-ERROR_RESULT_FQN = "/vol0/psych_audio/ahaque/self_harm/error_types.txt"
+ERROR_RESULT_FQN = "self_harm/error_types.txt"
