87ec8fe4426a0d8224e30a5e98fc841b62ff1a5c
Albert Haque <albert@alberthaque.com>
2020-06-03 00:54:55 -0700

    Clean up final files.

--- gcloud/config.py
+++ gcloud/config.py
@@ -1,6 +1,5 @@
 # Full path to the Google API key.
-# KEY = '/vol0/psych_audio/Google/keyAdam.json'
-KEY = "/vol0/psych_audio/ahaque/psych-audio/keyAdam.json"
+KEY = "your_key.json"
 
 # Upload the data to this Google cloud bucket.
-BUCKET_NAME = "phi_audio"
+BUCKET_NAME = "your_bucket"
